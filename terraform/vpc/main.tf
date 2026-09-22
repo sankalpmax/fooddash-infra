0cf99ed85afb89d9d4296265468508641b688c6d
sankalpmax <sankalparava619@gamail.com>
2026-09-22 16:29:30 +0530

    adding VPC Subnet terrafrom files for the fooddash application

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc" "food_app_vpc" {
+    cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform_app_vpc"
+    Project = "fooddash"
+  }
+}
+
+resource "aws_subnet" "pub_subnet_az_2a"{
+    vpc_id = aws_vpc.food_app_vpc.id
+    cidr_block = "10.0.1.0/24"  
+    availability_zone = "ap-south-2a"
+
+    tags = {
+        Name = "terraform_pub_subnet_az_2a"
+        Project = "fooddash"
+    }
+}
+
+resource "aws_subnet" "private_subnet_az_2a"{
+    vpc_id = aws_vpc.food_app_vpc.id
+    cidr_block = "10.0.2.0/24" 
+    availability_zone = "ap-south-2a"
+
+    tags = {
+        Name = "terraform_private_subnet_az_2a"
+        Project = "fooddash"
+    }
+}
+
+resource "aws_subnet" "pub_subnet_az_2b"{
+    vpc_id = aws_vpc.food_app_vpc.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "ap-south-2b"
+
+    tags = {
+        Name = "terraform_pub_subnet_az_2b"
+        Project = "fooddash"
+    }
+}
+
+resource "aws_subnet" "private_subnet_az_2b"{
+    vpc_id = aws_vpc.food_app_vpc.id
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "ap-south-2b"
+
+    tags = {
+        Name = "terraform_private_subnet_az_2b"
+        Project = "fooddash"
+    }
+}
+
+
