348e84dd215706d67397d2c266e786456d1b2586
sankalpmax <sankalparava619@gamail.com>
2026-09-24 15:33:46 +0530

    I have added the route table and the associations for both the az's

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,3 +1,4 @@
+## VPC Section ##
 resource "aws_vpc" "food_app_vpc" {
     cidr_block = "10.0.0.0/16"
   tags = {
@@ -6,6 +7,8 @@ resource "aws_vpc" "food_app_vpc" {
   }
 }
 
+
+## IGW Section ##
 resource "aws_internet_gateway" "fooddash" {
     vpc_id = aws_vpc.food_app_vpc.id
       
@@ -14,6 +17,10 @@ resource "aws_internet_gateway" "fooddash" {
       }
 }
 
+
+## Public Route Table and it's Association Section ##
+
+
 resource "aws_route_table" "food_app_public_route" {
     vpc_id = aws_vpc.food_app_vpc.id
   route  {
@@ -21,6 +28,43 @@ resource "aws_route_table" "food_app_public_route" {
     gateway_id =  aws_internet_gateway.fooddash.id
   }
 }
+
+resource "aws_route_table_association" "terraform_public_route_2a" {
+    route_table_id = aws_route_table.food_app_public_route.id
+    subnet_id = aws_subnet.pub_subnet_az_2a.id
+  
+}
+
+resource "aws_route_table_association" "terraform_public_route_2b" {
+    route_table_id = aws_route_table.food_app_public_route.id
+    subnet_id = aws_subnet.pub_subnet_az_2b.id
+  
+}
+
+## Private Route Table and Associations ##
+## Private route table - No routes for now 
+## NAT Gateway route 0.0.0.0/0 will be added in the future
+resource "aws_route_table" "terraform_private_route" {
+    vpc_id = aws_vpc.food_app_vpc.id
+    
+    tags = {
+      Name = "terraform_private_route"
+    }
+  
+}
+
+resource "aws_route_table_association" "terraform_private_subnet_az_2a" {
+    route_table_id = aws_route_table.terraform_private_route.id
+    subnet_id = aws_subnet.private_subnet_az_2a.id
+}
+
+resource "aws_route_table_association" "terraform_private_subnet_az_2b" {
+    route_table_id = aws_route_table.terraform_private_route.id
+    subnet_id = aws_subnet.private_subnet_az_2b.id
+  
+}
+## Subnets's Section ##
+
 
 resource "aws_subnet" "pub_subnet_az_2a"{
     vpc_id = aws_vpc.food_app_vpc.id
